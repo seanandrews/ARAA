3fd6cef33b9ab0c93c215dffeec8ebf95609fb19
Sean Andrews <sandrews@cfa.harvard.edu>
2019-07-31 09:22:52 -0400

    plot spectral indices

--- alphamm_dist.py
+++ alphamm_dist.py
@@ -2,11 +2,11 @@ import numpy as np
 import os
 import sys
 from astropy.io import ascii
-from plx_dist import plx_dist
-from scipy import stats
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from do_regression import do_regression
+plt.rc('font', size=9)
 
-nsamples  = 100000
-CI_levs = [84.135, 15.865, 95.45, 4.55]
 
 # for safety, copy over database file
 os.system('cp -r DISKS.csv temp.csv')
@@ -14,61 +14,61 @@ os.system('cp -r DISKS.csv temp.csv')
 # now load database
 db = ascii.read('temp.csv', format='csv', fast_reader=True)
 
-a67, ea67_hi, ea67_lo = np.zeros(len(db)), np.zeros(len(db)), np.zeros(len(db))
-fl_a67, lim_a67 = np.zeros(len(db)), np.zeros(len(db))
-a36, ea36_hi, ea36_lo = np.zeros(len(db)), np.zeros(len(db)), np.zeros(len(db))
-fl_a36, lim_a36 = np.zeros(len(db)), np.zeros(len(db))
+# set up plots
+fig = plt.figure(figsize=(6.33, 2.4))
+gs = gridspec.GridSpec(1, 2)
+Alims = [0., 4.]
+Mlims = [0.00125, 80.]
 
-for i in range(len(db)):
-    
-    # no index information available (no measurement in 1 band, or upper limits
-    # in both bands)
-    if (db['FL_B7'][i] == -1 or db['FL_B6'][i] == -1):
-        fl_a67[i], a67[i], ea67_hi[i], ea67_lo[i], lim_a67[i] = -1, 0, 0, 0, 0
-    if (db['FL_B6'][i] == -1 or db['FL_B3'][i] == -1):
-        fl_a36[i], a36[i], ea36_hi[i], ea36_lo[i], lim_a36[i] = -1, 0, 0, 0, 0
-    if (db['FL_B7'][i] == 1 and db['FL_B6'][i] == 1):
-        fl_a67[i], a67[i], ea67_hi[i], ea67_lo[i], lim_a67[i] = -1, 0, 0, 0, 0
-    if (db['FL_B6'][i] == 1 and db['FL_B3'][i] == 1):
-        fl_a36[i], a36[i], ea36_hi[i], ea36_lo[i], lim_a36[i] = -1, 0, 0, 0, 0
+ax0 = fig.add_subplot(gs[0, 0])
+ax0.set_xlim(Alims)
+ax0.set_ylim([0, 1])
+ax0.set_xlabel('$\\alpha_{\\rm mm}$')
+ax0.set_ylabel('frac')
 
-    # dual-band detections 
-    if (db['FL_B7'][i] == 0 and db['FL_B6'][i] == 0):
-        fl_a67[i], lim_a67[i] = 0, 0
-        a67[i] = np.log(db['F_B7'][i]/db['F_B6'][i]) / \
-                 np.log(db['nu_B7'][i]/db['nu_B6'][i])
-    if (db['FL_B6'][i] == 0 and db['FL_B3'][i] == 0):
-        fl_a36[i], lim_a36[i] = 0, 0
-        a36[i] = np.log(db['F_B6'][i]/db['F_B3'][i]) / \
-                 np.log(db['nu_B6'][i]/db['nu_B3'][i])
 
-    # single-band detections for *lower* limits on alpha
-    if (db['FL_B7'][i] == 0 and db['FL_B6'][i] == 1):
-        fl_a67[i], a67[i] = 1, -1
-        lim_a67[i] = np.log(db['F_B7'][i]/db['LIM_B6'][i]) / \
-                     np.log(db['nu_B7'][i]/db['nu_B6'][i])
-    if (db['FL_B6'][i] == 0 and db['FL_B3'][i] == 1):
-        fl_a36[i], a36[i] = 1, -1
-        lim_a36[i] = np.log(db['F_B6'][i]/db['LIM_B3'][i]) / \
-                     np.log(db['nu_B6'][i]/db['nu_B3'][i])
+# base selections
+base = ( (db['FL_MULT'] != 'B') & (db['FL_MULT'] != 'T') & \
+         (db['FL_MULT'] != 'J') & (db['FL_MULT'] != 'CB') & \
+         (db['SED'] != 'III') & (db['SED'] != 'DEBRIS') ) 
 
-    # single-band detections for *upper* limits on alpha
-    if (db['FL_B7'][i] == 1 and db['FL_B6'][i] == 0):
-        fl_a67[i], a67[i] = 2, -1
-        lim_a67[i] = np.log(db['LIM_B7'][i]/db['F_B6'][i]) / \
-                     np.log(db['nu_B7'][i]/db['nu_B6'][i])
-    if (db['FL_B6'][i] == 1 and db['FL_B3'][i] == 0):
-        fl_a36[i], a36[i] = 2, -1
-        lim_a36[i] = np.log(db['LIM_B6'][i]/db['F_B3'][i]) / \
-                     np.log(db['nu_B6'][i]/db['nu_B3'][i])
 
-# add the indices as columns to the database
-db['FL_A67'] = fl_a67
-db['A67'] = a67
-db['LIM_A67'] = lim_a67
-db['FL_A36'] = fl_a36
-db['A36'] = a36
-db['LIM_A36'] = lim_a36
+# B6/B7 indices
+ok67 = ((db['FL_A67'] == 0) & base)
+names = db['NAME'][ok67]
+num67 = len(names)
+a67_samples = []
+for i in range(num67):
+    a67_ = np.load('outputs/'+names[i]+'.alpha67.posterior.npz')['amm']
+    a67_samples = np.append(a67_samples, a67_)
 
-# write out the modified database
-ascii.write(db, 'temp.csv', format='csv', fast_writer=True, overwrite=True)
+
+ok36 = ((db['FL_A36'] == 0) & base)
+names = db['NAME'][ok36]
+num36 = len(names)
+a36_samples = []
+for i in range(num36):
+    a36_ = np.load('outputs/'+names[i]+'.alpha36.posterior.npz')['amm']
+    a36_samples = np.append(a36_samples, a36_)
+print(num36)
+
+
+# plot the histograms
+N, bins, patches = ax0.hist(a67_samples, range=[0, 4], bins=100, density=True,
+                            color='C0', align='mid', histtype=u'step', 
+                            linewidth=1.5)
+xx = np.linspace(0, 4, 1024)
+yy = 0.6*np.exp(-0.5*((xx-2.3)/0.6)**2)
+ax0.plot(xx, yy, 'r')
+#ax0.plot([2.3, 2.3], [0, 1], '--k')
+
+N, bins, patches = ax0.hist(a36_samples, range=[0, 4], bins=100, density=True,
+                            color='C1', align='mid', histtype=u'step', 
+                            linewidth=1.5)
+#ax0.plot([2.5, 2.5], [0, 1], '--k')
+
+
+fig.subplots_adjust(wspace=0.31, hspace=0.0)
+fig.subplots_adjust(left=0.09, right=0.91, bottom=0.17, top=0.98)
+fig.savefig('alphamm_dist.pdf')
+fig.clf()
