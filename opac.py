2d26821b3c2abcee8d1a34aeb9af82a3891f8d96
Sean Andrews <sandrews@SI385591.fios-router.home>
2019-08-14 21:32:41 -0400

    opacity figure updates

--- opac.py
+++ opac.py
@@ -1,17 +1,22 @@
 import numpy as np
+import os
+import sys
+
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-import sys
-import os
-plt.rc('font', size=9)
+plt.style.use('araa')
+from matplotlib import rc
+rc('text.latex', preamble=r'\usepackage{amsmath}')
+rc("font", **{"family": "serif", "serif": ["Palatino"]})
+rc("text", usetex = True)
 
 
-# set up the figure
-fig = plt.figure(figsize=(6.33, 3.3))
+# set up plot
+fig = plt.figure(figsize=(6.33, 3.2))
 gs = gridspec.GridSpec(2, 2)
-amax_lims = [1e-4, 100]
-kabs_lims = [0.02, 5]
-beta_lims = [0, 4]
+amax_lims = [1.25e-2, 80]
+kabs_lims = [0.05, 5]
+beta_lims = [0, 3.7]
 albp_lims = [-0.1, 1.1]
 
 ax0 = fig.add_subplot(gs[0, 0])
@@ -21,16 +26,18 @@ ax0.set_ylim(kabs_lims)
 ax0.set_yscale('log')
 ax0.set_yticks([0.1, 1])
 ax0.set_yticklabels(['0.1', '1'])
-ax0.set_ylabel('$\kappa_\\nu$  (cm$^2$ g$^{-1}$)')
+ax0.set_ylabel('$\kappa_\\nu \;$ (cm$^2$ g$^{-1}$)')
 ax0.set_xticklabels([])
 
 ax1 = fig.add_subplot(gs[1, 0])
 ax1.set_xlim(amax_lims)
 ax1.set_xscale('log')
 ax1.set_ylim(beta_lims)
-ax1.set_xlabel('$a_{\\rm max}$ (cm)')
+ax1.set_xticks([0.1, 1, 10])
+ax1.set_xticklabels(['0.1', '1', '10'])
+ax1.set_xlabel('$a_{\\rm max} \;$ (mm)')
 ax1.set_yticks([0, 1, 2, 3])
-ax1.set_ylabel('$\\beta$')
+ax1.set_ylabel('$\\beta$', labelpad=12)
 
 ax2 = fig.add_subplot(gs[0, 1])
 ax2.set_xlim(amax_lims)
@@ -38,46 +45,48 @@ ax2.set_xscale('log')
 ax2.set_ylim(albp_lims)
 ax2.set_xticklabels([])
 ax2.set_yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
-ax2.set_xlabel('$a_{max}$ (cm)')
 ax2.set_ylabel('$\omega_\\nu$')
 
 ax3 = fig.add_subplot(gs[1, 1])
 ax3.set_xlim(amax_lims)
 ax3.set_xscale('log')
+ax3.set_xticks([0.1, 1, 10])
+ax3.set_xticklabels(['0.1', '1', '10'])
 ax3.set_ylim(albp_lims)
 ax3.set_yticks([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])
-ax3.set_xlabel('$a_{\\rm max}$ (cm)')
+ax3.set_xlabel('$a_{\\rm max} \;$ (mm)')
 ax3.set_ylabel('$\mathcal{P}_\\nu$')
 
 
 
 # set up loop through data
 dfiles = ['q2.5_por0.5', 'q3.5_por0.5', 'q2.5_compact', 'q3.5_compact']
-lstys  = ['--C0', '--C3', 'C0', 'C3']
+lstys  = ['--C0', '--C1', 'C0', 'C1']
 
 
 for i in range(len(dfiles)):
 
     # load the data
     dat = np.load('data/dsharp_'+dfiles[i]+'.npz')
-    acm, wl = dat['acm'], dat['wl']
+    acm, wl = 10.*dat['acm'], dat['wl']
     ka, ks, alb, pol = dat['kabs'], dat['ksca'], dat['albedo'], dat['pol']
 
     # mm absorption opacities
-    ax0.plot(acm, ka[:, 1], lstys[i])
+    ax0.plot(acm, ka[:, 1], lstys[i], linewidth=2)
 
     # absorption opacity slope (beta)
-    ax1.plot(acm, np.log(ka[:, 1]/ka[:, 2]) / np.log(wl[2]/wl[1]), lstys[i])
+    ax1.plot(acm, np.log(ka[:, 1]/ka[:, 2]) / np.log(wl[2]/wl[1]), lstys[i],
+             linewidth=2)
 
     # albedo
-    ax2.plot(acm, alb[:, 1], lstys[i])
+    ax2.plot(acm, alb[:, 1], lstys[i], linewidth=2)
 
     # polarization 
-    ax3.plot(acm, pol[:, 1], lstys[i])
+    ax3.plot(acm, np.abs(pol[:, 1]), lstys[i], linewidth=2)
 
 
 
-fig.subplots_adjust(wspace=0.3, hspace=0.03)
-fig.subplots_adjust(left=0.08, right=0.92, bottom=0.12, top=0.99)
+fig.subplots_adjust(wspace=0.37, hspace=0.00)
+fig.subplots_adjust(left=0.1, right=0.9, bottom=0.13, top=0.98)
 fig.savefig('opac.pdf')
 fig.clf()
